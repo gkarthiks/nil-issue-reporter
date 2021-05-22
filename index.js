47bc2861d022880f12a533ffd10e726ff03e74f7
gkarthiks <github.gkarthiks@gmail.com>
2021-05-22 15:39:21 -0700

    fix
    Signed-off-by: gkarthiks <github.gkarthiks@gmail.com>

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const fs = require('fs');
 try {
     var eventName = github.context.eventName
     if (eventName.startsWith("issue")) {
+        var githubToken = core.getInput('token');
         var nilFileLoc = core.getInput("nil-file").trim();
         core.info("The NIL file picked up for comparative scan is from: "+nilFileLoc)
         var nilFileData = fs.readFileSync(nilFileLoc, 'utf8');
-
         var nilWordArray = nilFileData.toLocaleLowerCase().split(',');
-        core.info(nilWordArray)
+
+
+        core.info(github.context.payload);
+
 
         // Parsing the issue
-
         var issueContext = github.context.payload.issue.body;
         var issueNumber = github.context.payload.issue.number;
         var issueTitle = github.context.payload.issue.title;
@@ -24,8 +26,8 @@ try {
         // Create RegEx for parsing the data and comparing the nil
         var regEx = new RegExp(nilWordArray.join('|'), 'gi');
 
-        core.info("============================================================");
-        core.info(issueContext.toLocaleLowerCase().match(regEx));
+        validateAndComment(issueTitle, regEx, githubToken);
+        validateAndComment(issueContext, regEx, githubToken);
     }
 
 
@@ -40,4 +42,31 @@ try {
 //   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+// Validates the provided string for non-inclusive language
+function validateAndComment(stringToValidate, regEx, githubToken) {
+    core.info("Validating the given string for non-inclusive language with regEx: "+regEx);
+    var matchedNIL = stringToValidate.toLocaleLowerCase().match(regEx);
+
+    if (matchedNIL != null && matchedNIL.length > 0) {
+        core.info("Got the following non-inclusive language in the context: "+matchedNIL);
+        var bodyString = "Hi, you have the following non-inclusive language in the issue, please rephrase the sentence with inclusive language. Refer https://inclusivenaming.org/language/word-list/";
+        commentToIssue(bodyString, githubToken);
+    } else {
+        core.info("Hurray! The content is completely inclusive!!!");
+    }
+
+}
+
+
+// Commenting back to issue with provided message
+function commentToIssue(body, githubToken) {
+    github.getOctokit(githubToken).issues.createComment({
+        issue_number: github.context.issue.number,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        body: body
+    });
 }
